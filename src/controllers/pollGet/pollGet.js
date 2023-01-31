81198bc6f324d771d2e58097a85d7d2291994f38
Marcio-VOT <marciofsantos65@gmail.com>
2023-01-31 13:53:50 -0300

    feat: /get de polls implementado

--- src/controllers/pollGet/pollGet.js
+++ src/controllers/pollGet/pollGet.js
@@ -2,6 +2,8 @@ import { pollCollection } from "../../config/database.js";
 
 export default async (req, res) => {
   try {
+    const poll = pollCollection.find({}).toArray();
+    res.send(poll);
   } catch (error) {
     res.status(500).send(error.message);
   }
